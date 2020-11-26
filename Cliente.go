f7ba3d351df2d139406e5ff1d2d309070b8b13e0
Fralkayg <csepulvedav@outlook.com>
2020-11-26 17:19:33 -0300

    asdasdasdsdasda

--- Cliente.go
+++ Cliente.go
@@ -2,8 +2,13 @@ package main
 
 import (
 	"context"
-	"log"
 	"fmt"
+	"io/ioutil"
+	"log"
+	"math"
+	"os"
+	"strconv"
+
 	pb "./Service"
 	"google.golang.org/grpc"
 )
@@ -22,12 +27,63 @@ func helloWorld(conn *grpc.ClientConn) {
 	fmt.Println("Mensaje: ", response.Mensaje)
 }
 
+func splitFile(file string) int {
+	fileToBeChunked := "./Books/" + file // change here!
+
+	file, err := os.Open(fileToBeChunked)
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	defer file.Close()
+
+	fileInfo, _ := file.Stat()
+
+	var fileSize int64 = fileInfo.Size()
+
+	const fileChunk = 250000 // 1 MB, change this to your requirement
+
+	// calculate total number of parts the file will be chunked into
+
+	totalPartsNum := uint64(math.Ceil(float64(fileSize) / float64(fileChunk)))
+
+	fmt.Printf("Splitting to %d pieces.\n", totalPartsNum)
+
+	for i := uint64(0); i < totalPartsNum; i++ {
+
+		partSize := int(math.Min(fileChunk, float64(fileSize-int64(i*fileChunk))))
+		partBuffer := make([]byte, partSize)
+
+		file.Read(partBuffer)
+
+		// write to disk
+		fileName := "./Client/Chunks/" + file + "_" + strconv.FormatUint(i+1, 10)
+		_, err := os.Create(fileName)
+
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		// write/save buffer to disk
+		ioutil.WriteFile(fileName, partBuffer, os.ModeAppend)
+		fmt.Println("Split to : ", fileName)
+	}
+	return int(totalPartsNum)
+}
+
 func main() {
 	conn, err := grpc.Dial(address, grpc.WithInsecure(), grpc.WithBlock())
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
 	}
 	defer conn.Close()
+
+	totalParts := splitFile("./Books/El_maravilloso_Mago_de_Oz-L._Frank_Baum.pdf")
+
+	fmt.Println("Se dividio el archivo en : ", totalParts)
 
 	helloWorld(conn)
 
