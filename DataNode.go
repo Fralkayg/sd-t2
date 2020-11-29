542e1a1214beafafdee5a8ca94aa640273911538
Fralkayg <csepulvedav@outlook.com>
2020-11-29 19:29:45 -0300

    d e b u g

--- DataNode.go
+++ DataNode.go
@@ -492,7 +492,7 @@ func generateCentralizedDistribution(s *server) {
 		}
 	}
 	if distributionReply.Machines[2].Status == 1 {
-		fmt.Println(distributionReply.Machines[2].Distribution)
+		// fmt.Println(distributionReply.Machines[2].Distribution)
 		fmt.Println(len(distributionReply.Machines[2].Distribution))
 		for j := 0; j < len(distributionReply.Machines[2].Distribution); j++ {
 			fileName := distributionReply.FileName + "_" + strconv.Itoa(int(s.file.chunks[distributionReply.Machines[2].Distribution[j]].ChunkIndex))
